ed0e1958ebac8b450a0ef3a4c8b5c19e6ab6d521
matthew.bates <matthew.bates@LinkMJB.phx.connexta.com>
2018-10-29 00:51:40 -0700

    Added auto-padding, a few more spinners, and randomization examples

--- spinner.sh
+++ spinner.sh
@@ -21,6 +21,28 @@ set_spinner() {
       SPINNER=(":(" ":|" ":)" ":D")
       SPINNER_INTERVAL=0.5
       ;;    
+    spinner5)
+      SPINNER=("^" ">" "v" "<")
+      SPINNER_INTERVAL=0.5
+      ;;    
+    spinner6)
+      SPINNER=("<('-'<)" "^('-')^" "(>'-')>" "v('-')v")
+      SPINNER_INTERVAL=0.5
+      ;;    
+    spinner7)
+      SPINNER=(`x=1; while [ ${x} -le 10 ]; do echo $((${RANDOM} % ${x})); x=$((${x}+1)); done`)
+      SPINNER_INTERVAL=0.3
+      ;;    
+    spinner8)
+      SPINARR=("^" ">" "v" "<")
+      SPINNER=(`x=1; while [ ${x} -le 20 ]; do echo ${SPINARR[$((${RANDOM} % ${#SPINARR[@]}))]}; x=$((${x}+1)); done`)
+      SPINNER_INTERVAL=0.1
+      ;;    
+    spinner9)
+      SPINARR=("<('-'<)" "^('-')^" "(>'-')>" "v('-')v")
+      SPINNER=(`x=1; while [ ${x} -le 20 ]; do echo ${SPINARR[$((${RANDOM} % ${#SPINARR[@]}))]}; x=$((${x}+1)); done`)
+      SPINNER_INTERVAL=0.1
+      ;;    
     *)
       echo "No spinner is defined for $1"
       exit 1
@@ -35,8 +57,10 @@ start() {
   while [ "$step" -lt "${#CMDS[@]}" ]; do
     ${CMDS[$step]} & pid=$!
 
+    SPACES=`echo ${SPINNER[0]} | sed 's/./ /g'`
+
     while ps -p $pid &>/dev/null; do
-      echo -ne "\\r[   ] ${STEPS[$step]} ..."
+      echo -ne "\\r[ ${SPACES} ] ${STEPS[$step]} ..."
 
       for k in "${!SPINNER[@]}"; do
         echo -ne "\\r[ ${SPINNER[k]} ]"
@@ -44,7 +68,7 @@ start() {
       done
     done
 
-    echo -ne "\\r[ ✔ ] ${STEPS[$step]}\\n"
+    echo -ne "\\r[ ✔ ] ${STEPS[$step]} ${SPACES}   \\n"
     step=$((step + 1))
   done
 
